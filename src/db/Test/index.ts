fecc17f85b234173d96ea39efdf1bffc7b10f28b
walterdiazesa <walterwalon@gmail.com>
2022-05-14 22:02:55 -0600

    Revalidate test/:id on createTest

--- src/db/Test/index.ts
+++ src/db/Test/index.ts
@@ -1,4 +1,5 @@
 import { Prisma } from "@prisma/client";
+import axios from "axios";
 import { prisma } from "../handler.js";
 
 interface TestItem {
@@ -32,10 +33,38 @@ export async function getTests({
 
 export async function createTest(data: Test) {
   const json = data.tests as unknown as Prisma.JsonArray;
-  return await prisma.test.create({
+  const test = await prisma.test.create({
     data: {
       ...data,
       tests: json,
     },
   });
+  try {
+    const { status } = await axios.get(
+      `${process.env.APP_HOST}/api/revalidatetest`,
+      {
+        params: {
+          test: test.id,
+          revalidate_token: process.env.REVALIDATE_TOKEN,
+        },
+      }
+    );
+    if (status === 500)
+      console.error(`revalidation on /tests/${test.id} failed!`);
+    if (status === 401)
+      console.error(`invalid token for revalidating /tests/${test.id}`);
+  } catch (e) {
+    if (e.response) {
+      if (e.response.status === 500)
+        console.error(`revalidation on /tests/${test.id} failed!`);
+      if (e.response.status === 401)
+        console.error(`invalid token for revalidating /tests/${test.id}`);
+    } else {
+      console.error(
+        `Error on createTest, revalidating test/${test.id}`,
+        e.message
+      );
+    }
+  }
+  return test;
 }

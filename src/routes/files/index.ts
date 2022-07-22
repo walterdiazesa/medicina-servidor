c7ae59b112893e9691e6906f57355e57cb41ea0a
walterdiazesa <walterwalon@gmail.com>
2022-07-21 18:21:17 -0600

    test x cors

--- src/routes/files/index.ts
+++ src/routes/files/index.ts
@@ -6,12 +6,17 @@ import {
 
 const router = Router();
 
-router.get("/upload", async (req, res) =>
-  res
-    .status(200)
-    .send(
-      await getFileUploadUrl("public-files", EXPIRE_PUBLIC_FILES_BUCKET_SECONDS)
-    )
-);
+router.get("/upload", async (req, res) => {
+  console.log(
+    "New request to /files/upload",
+    req.headers.origin,
+    req.headers.host
+  );
+  if (req.headers.origin !== process.env.APP_HOST)
+    return res.status(403).send("No estás authorizado lmao");
+  res.send(
+    await getFileUploadUrl("public-files", EXPIRE_PUBLIC_FILES_BUCKET_SECONDS)
+  );
+});
 
 export default router;

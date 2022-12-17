2bacac66ff593cd64743011ac16fa56d759b17d3
Walter Alejandro Díaz Portillo <wdiazportillo@shutterstock.com>
2022-12-17 01:34:45 +0100

    + Temp routesGuard

--- src/routes/middlewares/security/index.ts
+++ src/routes/middlewares/security/index.ts
@@ -7,6 +7,16 @@ export function routesGuard(
   next: NextFunction
 ) {
   if (process.env.NODE_ENV.trim() !== "DEV") {
+    console.log(
+      "\x1b[34m🔒 routesGuard \x1b[35m(src/routes/middlewares/security/index.ts)\x1b[0m",
+      req.path,
+      req.method,
+      req.headers.origin,
+      req.get("origin"),
+      req.ip,
+      req.headers["access-control-allow-origin"],
+      req.rawHeaders
+    );
     const isDomainAllowed = corsWhiteList.includes(req.headers.origin);
     const isOperationAllowed = (path: string, method: string) => {
       if (!isDomainAllowed) {
